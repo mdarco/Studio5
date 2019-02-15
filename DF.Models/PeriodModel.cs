a23e084eef3182114d079f87caa694ac2618b293
Darko Milutinović <dmilutinovic@gmail.com>
2019-02-15 23:35:26 +0100

    added Periods model and DAL class

---
+++ DF.Models/PeriodModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DF.Models
+{
+    public class PeriodModel
+    {
+        public int PeriodID { get; set; }
+        public string PeriodName { get; set; }
+        public string PeriodDesc { get; set; }
+        public string WeekDay { get; set; }
+        public TimeSpan? StartTime { get; set; }
+        public TimeSpan? EndTime { get; set; }
+    }
+}
