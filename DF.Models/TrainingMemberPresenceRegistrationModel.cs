4153ccbcdae1642a5e2611cd09ad7a58128add15
Darko Milutinović <dmilutinovic@gmail.com>
2019-08-29 20:28:24 +0200

    added training models

---
+++ DF.Models/TrainingMemberPresenceRegistrationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DF.Models
+{
+    public class TrainingMemberPresenceRegistrationModel
+    {
+        public int TrainingID { get; set; }
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        public bool IsPresent { get; set; }
+        public bool? AbsenceJustified { get; set; }
+        public string AbsenceNote { get; set; }
+    }
+}
