00d04f73333db90d9863490102d08010c380e30b
Darko Milutinovic <dmilutinovic@gmail.com>
2020-04-05 14:41:13 +0200

    fox for document previewer - blobUrl

--- DF.Web/pages/common/doc-preview-dialog/DocPreviewDialogController.js
+++ DF.Web/pages/common/doc-preview-dialog/DocPreviewDialogController.js
@@ -5,14 +5,19 @@
         .module('DFApp')
         .controller('DocPreviewDialogController', ctrlFn);
 
-    ctrlFn.$inject = ['$scope', '$uibModalInstance', 'toastr', 'settings'];
+    ctrlFn.$inject = ['$scope', '$uibModalInstance', '$sce', 'UtilityService', 'toastr', 'settings'];
 
-    function ctrlFn($scope, $uibModalInstance, toastr, settings) {
+    function ctrlFn($scope, $uibModalInstance, $sce, UtilityService, toastr, settings) {
         $scope.docUrl = '';
 
         if (settings && settings.DocUrl) {
             $scope.docUrl = settings.DocUrl;
+            $scope.blobUrl = window.URL.createObjectURL(UtilityService.dataURItoBlob($scope.docUrl));
         }
+
+        $scope.getIframeSrc = function (blobUrl) {
+            return $sce.trustAsResourceUrl(blobUrl);
+        };
 
         $scope.close = function () {
             $uibModalInstance.dismiss();
